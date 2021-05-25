93b8ef5d7816bc77de4eb4b196a8d3186db24ce7
andriansyah7 <dwiandriansyah7@gmail.com>
2021-05-25 15:52:25 +0700

    commit script by dwi

--- resources/views/Template/script.blade.php
+++ resources/views/Template/script.blade.php
@@ -16,11 +16,6 @@
 <!-- Bootstrap4 Duallistbox -->
 <script src="{{asset('adminLTE')}}/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
 
-<!-- date-range-picker -->
-<script src="{{asset('adminLTE')}}/plugins/daterangepicker/daterangepicker.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.38.0/js/tempusdominus-bootstrap-4.min.js" crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.0/moment.min.js"></script>
 <!-- DataTables  & Plugins -->
 <script src="{{asset('adminLTE')}}/plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="{{asset('adminLTE')}}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
@@ -34,10 +29,6 @@
 <script src="{{asset('adminLTE')}}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
 <script src="{{asset('adminLTE')}}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
 <script src="{{asset('adminLTE')}}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 <!-- page script -->
 <script type="text/javascript">
